2782f3f7ab2e8773f611c93435f613bc18a1369c
RicardoNunes <1232075@isep.ipp.pt>
2024-12-14 11:24:30 +0000

    apagar

--- src/NA_1232075_1232123.java
+++ src/NA_1232075_1232123.java
@@ -15,6 +15,8 @@ public class NA_1232075_1232123 {
         // a
         int[][] matrizPlaneamento = obterInformacao();
         // b
+
+        System.out.println("apagar linha");
         somarKmTotalAPercorrerPorCadaVeiculo(matrizPlaneamento);
         // c
         int[][] matrizCarregamentos = calcularNumeroRecargasDaBateriaPorDiaPorCarro(matrizPlaneamento);
