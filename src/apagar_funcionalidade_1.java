e793a9d9be6be4348849071538dd1f69cbfd3ce2
RicardoNunes <1232075@isep.ipp.pt>
2024-12-15 14:59:48 +0000

    .

--- src/apagar_funcionalidade_1.java
+++
@@ -1,5 +0,0 @@
-public class apagar_funcionalidade_1 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/apagar_funcionalidade_2.java
+++
@@ -1,5 +0,0 @@
-public class apagar_funcionalidade_2 {
-    public static void main(String[] args) {
-
-    }
-}
